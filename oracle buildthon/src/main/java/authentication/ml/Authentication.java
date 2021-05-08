d8bd2cd4401d6646d9a76c45176273b1d20ba2c1
prasuna5 <79966616+prasuna5@users.noreply.github.com>
2021-05-08 20:48:15 +0530

    Add files via upload

--- oracle buildthon/src/main/java/authentication/ml/Authentication.java
+++ oracle buildthon/src/main/java/authentication/ml/Authentication.java
@@ -2,6 +2,12 @@ package authentication.ml;
 
 import java.util.Arrays;
 
+import tech.tablesaw.api.Table;
+import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.components.Figure;
+import tech.tablesaw.plotly.components.Layout;
+import tech.tablesaw.plotly.traces.BoxTrace;
+import tech.tablesaw.plotly.traces.HistogramTrace;
 import weka.classifiers.Classifier;
 import weka.classifiers.evaluation.Evaluation;
 import weka.core.Instance;
@@ -29,15 +35,34 @@ public class Authentication {
 		return dataset;
 	}
 	
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws Exception{
-
-		Instances test_data = getInstances("C:\\\\Users\\\\Welcome\\\\OneDrive\\\\Desktop\\\\oracle buildthon\\\\src\\\\main\\\\java\\\\authentication\\\\ml\\\\testingdata.arff");
-		Instances train_data = getInstances("C:\\\\Users\\\\Welcome\\\\OneDrive\\\\Desktop\\\\oracle buildthon\\\\src\\\\main\\\\java\\\\authentication\\\\ml\\\\trainingdata.arff");
-		System.out.println(train_data.size());
+		System.out.println("Data Analysis");
+	
+		Table data = Table.read().csv("C:\\Users\\Welcome\\OneDrive\\Desktop\\oracle buildthon\\src\\main\\java\\authentication\\ml\\data_banknote_authentication.csv");
+		System.out.println(data.shape());
+		System.out.println(data.first(5));
+		System.out.println(data.last(5));
+		System.out.println(data.structure());
+		System.out.println(data.summary());
 		
-		/** Classifier here is Linear Regression */
+		//Histogram
+		Layout layout1 = Layout.builder().title("Distribution of Skewness").build();
+		HistogramTrace t1 = HistogramTrace.builder(data.nCol("Skewness")).build();
+		Plot.show(new Figure(layout1,t1));
+		
+		Layout l2 = Layout.builder().title("Class Distribution").build();
+		BoxTrace t2 = BoxTrace.builder(data.categoricalColumn("Class"),data.nCol("Skewness")).build();
+		Plot.show(new Figure(l2,t2));
+		System.out.println("-------------------------------------------------------------------------------------\n\n");
+		
+		Instances test_data = getInstances("C:\\Users\\Welcome\\OneDrive\\Desktop\\oracle buildthon\\src\\main\\java\\authentication\\ml\\test_data.arff");
+		Instances train_data = getInstances("C:\\Users\\Welcome\\OneDrive\\Desktop\\oracle buildthon\\src\\main\\java\\authentication\\ml\\train_data.arff");
+		System.out.println("Train dataset size"+train_data.size());
+		
+		//Building a classifier
 		Classifier classifier = new weka.classifiers.functions.Logistic();
-		/** */
+	
 		classifier.buildClassifier(train_data);
 		
 		
@@ -49,9 +74,7 @@ public class Authentication {
 		eval.evaluateModel(classifier, test_data);
 		/** Print the algorithm summary */
 		System.out.println("** Logistic Regression Evaluation with Datasets **");
-		System.out.println(eval.toSummaryString());
-//		System.out.print(" the expression for the input data as per alogorithm is ");
-//		System.out.println(classifier);
+		System.out.println(eval.toSummaryString());       
 		
 		double confusion[][] = eval.confusionMatrix();
 		System.out.println("Confusion matrix:");
@@ -77,14 +100,13 @@ public class Authentication {
 		double acc = eval.correct()/(eval.correct()+ eval.incorrect());
 		System.out.println(Math.round(acc*100.0)/100.0);
 		
-		
-		System.out.println("-------------------");
-		Instance predicationDataSet = test_data.get(2);
-		double value = classifier.classifyInstance(predicationDataSet);
+ 		System.out.println("-------------------");
+ 		Instances predict_data = getInstances("C:\\Users\\Welcome\\OneDrive\\Desktop\\oracle buildthon\\src\\main\\java\\authentication\\ml\\cross_validation.arff");
+		Instance predictionDataSet = predict_data.get(10); 		
+		double value = classifier.classifyInstance(predictionDataSet);
 		/** Prediction Output */
 		System.out.println("Predicted label:");
-		System.out.print(value);
-		
+		System.out.print(value);	
 		
 	}
 
